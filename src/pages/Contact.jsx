d466e03a0e20d9a2fef6f344d2bd53ec051bb8e3
Keshav Kumar <keshavk1002003@gmail.com>
2025-10-06 12:03:31 +0530

    --

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
     toast.loading("Submitting your enquiry...");
     try {
       await axios.post(
-        "http://localhost:5000/api/form/send",
+        "https://backend-cows-choice.onrender.com/api/form/send",
         formData
       );
       toast.dismiss();
